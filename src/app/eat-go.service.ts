407dfd71d95b114b0ae45e198475a083446ffe02
NaLi18 <nali@DESKTOP-RP97F9T.localdomain>
2019-10-25 05:32:07 -0400

    Add the get request method

--- src/app/eat-go.service.ts
+++ src/app/eat-go.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EatGoService {
+  API_KEY:string ="AIzaSyCFCDqcWZryyPAnPa8h_cJVAWemz-kOhYo"
+  constructor(private httpClient: HttpClient){ }
 
-  constructor() { }
+  public getRest(){
+    return this.httpClient.get<Config>(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Albany&key=${this.API_KEY}`);
+  }
+}
+
+export interface Config {
+  heroesUrl: string;
+  textfile: string;
 }
