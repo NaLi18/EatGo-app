6bdab14b9b5f6d1acb310ceca2aca015f1d62a24
NaLi18 <nali@DESKTOP-RP97F9T.localdomain>
2019-10-25 05:53:25 -0400

    Added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EatGoComponent } from './eat-go/eat-go.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'eatgo',component:EatGoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
