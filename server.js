b61e341bf76e80ab9e48cfdca495e730dbfdb840
NaLi18 <nali@DESKTOP-RP97F9T.localdomain>
2019-10-25 19:34:50 -0400

    Able to get the restaurant from different location

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const cors= require('cors')
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'dist', 'EatGo-app')))
 
-const googlePLacesQueryUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyCFCDqcWZryyPAnPa8h_cJVAWemz-kOhYo&query=restaurants+in+Albany"
-
-
-app.get('/api/eatgo', (req, res) => {
-    //const location = req.Config.place;
-    request.get(googlePLacesQueryUrl)
+app.get('/eatgo/:uid', (req, res) => {
+    const locat = req.params.uid
+    var googlePLacesQueryUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyCFCDqcWZryyPAnPa8h_cJVAWemz-kOhYo&query="
+    request.get( googlePLacesQueryUrl + "+restaurants+in+"+locat)
     .pipe(res);
 });
 
--- src/app/eat-go.service.ts
+++ src/app/eat-go.service.ts
@@ -11,10 +11,9 @@ export class EatGoService {
   constructor(private httpClient: HttpClient){ }
 
   public getRest(id:string){
-   // Config.heroesUrl = place;
     console.log("logging from service"+id);
-    return this.httpClient.get<Config>("http://localhost:3000/api/eatgo");
-    //return this.httpClient.get<Config>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e06f5b3da2be4501af757078cf03a985`);
+    return this.httpClient.get<Config>('http://localhost:3000/eatgo/'+id);
+  
   }
 }
 
